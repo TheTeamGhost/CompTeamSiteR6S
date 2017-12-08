51d30989d028104fa14cb5f94658cb52cec4ef58
Game Server <gameservers@localhost.localdomain>
2017-12-08 13:12:09 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Join the Team</title>
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="style/master.css">
+    </head>
+    <body>
+        <nav>
+            <div class="mainWrapper">
+                <ul>
+                    <li><a class="nav-items" href="index.php">Home</a></li>
+                    <li><a class="nav-items" href="index.php#team">The Team</a></li>
+                    <li><a class="nav-items" href="news.php">News</a></li>
+                    <li><a class="nav-items" href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+        <img class="banner" src="img/banner_header.png" alt="banner image">
+    </body>
+</html>
