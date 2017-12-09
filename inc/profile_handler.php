7dd62896b2edb80a43a48a85bf85a765cde375a1
Game Server <gameservers@localhost.localdomain>
2017-12-09 12:01:52 +0100

    Added most database handlers

---
+++ inc/profile_handler.php
@@ -0,0 +1,7 @@
+<?php
+    $user_name = 'SELECT username FROM users';
+    $quote = 'SELECT quote FROM users'
+    $bio = 'SELECT bio FROM users'
+    $steamid = 'SELECT steamid FROM users'
+    $user_profile_pic = 'SELECT profile-pic_filename FROM users'
+?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    require 'inc/db_connect.php';
+    include 'inc/profile_handler.php';
+    include 'inc/getnews.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -24,8 +29,9 @@
                     <div id="latest-news" uk-offcanvas="mode: push; overlay: true">
                         <div class="uk-offcanvas-bar">
                             <button class="uk-offcanvas-close" type="button" uk-close></button>
-                            <h3>$newsTitle</h3>
-                            <p>Some random bullshit ble ble ble ble</p>
+                            <h3>$news_title</h3>
+                            <p>$news_text</p>
+                            <div class="uk-card-badge uk-label">$news_date</div>
                         </div>
                     </div>
                     <li class="navbar-li"><a class="nav-items" href="login.php">Login</a></li>
@@ -37,7 +43,7 @@
             <ul uk-tab>
                 <li><a class="first-section-font" href="#">Flop</a></li>
                 <li><a class="first-section-font" href="#">Aspect</a></li>
-                <li><a class="first-section-font" href="#">Random1</a></li>
+                <li><a class="first-section-font" href="#">$user_name</a></li>
             </ul>
 
             <ul class="uk-switcher uk-margin">
@@ -75,13 +81,19 @@
                 </li>
                 <li>
                     <article class="uk-article">
-                        <img class="uk-align-right profile-pic" src="img/profiles/dummy-profile.png" alt="">
-                        <h1 class="uk-article-title"><a class="uk-link-reset" href="$userprofile">$username</a></h1>
-                        <p class="uk-text-lead">$shorttext</p>
-                        <p>$biotext</p>
+                        <img class="uk-align-right profile-pic" src="img/profiles/$user_profile_pic" alt="">
+                        <h1 class="uk-article-title"><a class="uk-link-reset" href="$userprofile">$user_name</a></h1>
+                        <p class="uk-text-lead">$quote</p>
+                        <p>$bio</p>
                         <div class="uk-grid-small uk-child-width-auto" uk-grid>
                             <div>
-                                <a class="uk-button uk-button-text" href="#">Site profile</a>
+                                <a class="uk-button uk-button-text" href="#">Site profile (in the works)</a><!--Will be added later on -->
+                            </div>
+                        </div>
+                        <div class="uk-grid-small uk-child-width-auto" uk-grid>
+                            <div>
+                                <a class="uk-button uk-button-text" href="http://steamcommunity.com/profiles/$steamid" target="_blank">Steam profile</a>
+                                <!--get steam link with php, http://steamcommunity.com/profiles/$steamid  $steamid staat in database -->
                             </div>
                         </div>
                     </article>
